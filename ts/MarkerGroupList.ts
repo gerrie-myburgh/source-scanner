0528c5e92a933ef414f59e1db0e0665b1575f79f
gerrie-myburgh <gerrie.myburgh@yahoo.co.uk>
2024-04-27 01:37:45 +0200

    Marker list bug fix

--- ts/MarkerGroupList.ts
+++ ts/MarkerGroupList.ts
@@ -19,11 +19,12 @@ export class MarkerGroupList {
         // some containers to use later on
         //
         const markerToDocumentMap = new Map<string, Set<string>>();
-        const allSolutionFiles = new Array<string>();
+        const markerToTestDocumentMap = new Map<string, Set<string>>();
         //
         // get all the doc files to scan
         //
         const commentFiles = this.utils.listMDFilesInVault(this.docFolders.settingsCommentFolder);
+        const testCommentFiles = this.utils.listMDFilesInVault(this.docFolders.settingsTestCommentFolder);
         //
         // pick up all markers in the doc string doc file by doc file and aggregate the markers
         // before processing them
@@ -41,7 +42,6 @@ export class MarkerGroupList {
                             markerSet.add(marker[0].trim());
                         })
                         markerSet = this.utils.sortSetOfString(markerSet);
-                        const documentName = commentFile.split('/').last() as string;
                         markerSet.forEach(marker => {
                             if (!markerToDocumentMap.has(marker.trim())) {
                                 markerToDocumentMap.set(marker, new Set<string>());
@@ -50,13 +50,32 @@ export class MarkerGroupList {
                         });
                     }));
         })
-
+        testCommentFiles.forEach(commentFile => {
+            promise.push(this.utils.fsa.read(commentFile)
+                .then(value => {
+                        var markerSet = new Set<string>();
+                        //
+                        // get all the markers in the value string
+                        //
+                        const markersMatch = value.matchAll(this.utils.markerRegExp);
+                        Array.from(markersMatch).forEach(marker => {
+                            markerSet.add(marker[0].trim());
+                        })
+                        markerSet = this.utils.sortSetOfString(markerSet);
+                        markerSet.forEach(marker => {
+                            if (!markerToTestDocumentMap.has(marker.trim())) {
+                                markerToTestDocumentMap.set(marker, new Set<string>());
+                            }
+                            markerToTestDocumentMap.get(marker.trim())?.add(commentFile);
+                        });
+                    }));
+        })
         Promise.allSettled(promise)
             .then(value => {
-                var allMarkers = new Set<string>(Array.from(markerToDocumentMap.keys()));
-                allMarkers = this.utils.sortSetOfString(allMarkers);
+                const allMarkers = Array.from(markerToDocumentMap.keys()).sort();
 
-                var mdString = `|marker|document|\n`;
+                var mdString = "Code and Code Story links\n\n";
+                mdString = mdString + `|marker|document|\n`;
                 mdString = mdString + `|------|--------|\n`;
                 allMarkers.forEach(marker => {
                     const docNameSet = markerToDocumentMap.get(marker);
@@ -67,6 +86,21 @@ export class MarkerGroupList {
                         mdString = mdString + `|${marker.substring(1)}|[[${docName}#${marker}]]\n`;
                     })
                 })
+
+                const allTestMarkers = Array.from(markerToTestDocumentMap.keys()).sort();
+
+                mdString = mdString + "\nTest and Test Story links\n\n";
+                mdString = mdString + `|marker|document|\n`;
+                mdString = mdString + `|------|--------|\n`;
+                allTestMarkers.forEach(marker => {
+                    const docNameSet = markerToTestDocumentMap.get(marker);
+                    //
+                    // build marker to test doc entry from the set of document names.
+                    //
+                    docNameSet?.forEach(docName => {
+                        mdString = mdString + `|${marker.substring(1)}|[[${docName}#${marker}]]\n`;
+                    })
+                })                
                 this.utils.fsa.write(this.markerFileWithPath, mdString);
             })
     }
